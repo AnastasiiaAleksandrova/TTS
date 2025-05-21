80e0c2360be9ed75af1be3fd5b67c44e07de423f
aleana <anastasiia.a.aleksandrova@gmail.com>
2025-05-21 14:04:33 +0300

    audio player

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,11 @@ import "./App.css";
 
 function App() {
   const [text, setText] = useState("");
+  const [audioFile, setAudioFile] = useState("");
 
-  const generateText = async () => {
-    const response = await fetch("http://localhost:8080/generate");
-    const data = await response.json();
-    setText(data.text);
-  };
-
-  const addMarkup = async () => {
-    const response = await fetch("http://localhost:8080/add-markup", {
-      method: "POST",
+  const createStory = async () => {
+    const response = await fetch("http://localhost:8080/create-and-play", {
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
@@ -21,22 +16,12 @@ function App() {
     });
     const data = await response.json();
     setText(data.text);
+    setAudioFile(data.audioFile);
   };
-  const generateSpeech = async () => {
-    const response = await fetch("http://localhost:8080/generate-speech", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ text }),
-    });
-    const data = await response.json();
-    const audio = new Audio(data.audioUrl);
-    audio.play();
-  };
+
   return (
     <>
-      <button style={{ margin: "5px" }} onClick={generateText}>
+      <button style={{ margin: "5px" }} onClick={createStory}>
         Create a story
       </button>
       <textarea
@@ -45,12 +30,13 @@ function App() {
       >
         {text}
       </textarea>
-      <button style={{ margin: "5px" }} onClick={addMarkup}>
-        Add markup
-      </button>
-      <button style={{ margin: "5px" }} onClick={generateSpeech}>
-        Generate speech
-      </button>
+      <audio
+        controls
+        style={{ width: "500px", display: "block", margin: "5px" }}
+        src={audioFile}
+      >
+        Your browser does not support the audio element.
+      </audio>
     </>
   );
 }
