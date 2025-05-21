003f5792e16520f49bae0011c0fd1b376e9a99ad
aleana <anastasiia.a.aleksandrova@gmail.com>
2025-05-21 13:43:16 +0300

    ui tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,46 @@ function App() {
     setText(data.text);
   };
 
+  const addMarkup = async () => {
+    const response = await fetch("http://localhost:8080/add-markup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text }),
+    });
+    const data = await response.json();
+    setText(data.text);
+  };
+  const generateSpeech = async () => {
+    const response = await fetch("http://localhost:8080/generate-speech", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text }),
+    });
+    const data = await response.json();
+    const audio = new Audio(data.audioUrl);
+    audio.play();
+  };
   return (
     <>
-      <button onClick={generateText}>Generate test</button>
-      <div>{text}</div>
-      <button>Enhance pronunciation</button>
+      <button style={{ margin: "5px" }} onClick={generateText}>
+        Create a story
+      </button>
+      <textarea
+        style={{ width: "500px", height: "500px", display: "block" }}
+        readOnly
+      >
+        {text}
+      </textarea>
+      <button style={{ margin: "5px" }} onClick={addMarkup}>
+        Add markup
+      </button>
+      <button style={{ margin: "5px" }} onClick={generateSpeech}>
+        Generate speech
+      </button>
     </>
   );
 }
